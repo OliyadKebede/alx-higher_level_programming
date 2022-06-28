24423c471d25068905bfee22ff8b4ea3c0831557
Oliyad Kebede <81267448+OliyadKebede@users.noreply.github.com>
2022-06-28 16:49:38 -0700

    Update 100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def magic_string():
     magic_string.n = getattr(magic_string, 'n', 0) + 1
-    return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")
-
-for i in range(4):
-    print(magic_string())
+    line = ("BestSchool, " * (magic_string.n - 1) + "BestSchool$")
+    return line
