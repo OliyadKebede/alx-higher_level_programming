332ce6317c199007b1b566be2ab35aa03a2a4e56
Oliyad Kebede <81267448+OliyadKebede@users.noreply.github.com>
2022-06-28 16:36:59 -0700

    Update 100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,15 +1,4 @@
 #!/usr/bin/python3
-
-
-def call_counter(func):
-    def helper():
-        helper.calls += 1
-        return func()
-    helper.calls = 0
-
-    return helper
-
-
-@call_counter
-def magic_string():
-    return 'BestSchool, ' * (magic_string.calls - 1) + 'BestSchool'
+def magic_string(l=[]):
+    l += ["BestSchool"]
+    return ", ".join(l)
