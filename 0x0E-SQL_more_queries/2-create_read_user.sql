16190e6552303dfcfddb8fc5108449dc390a0c22
Oliyad Kebede <81267448+OliyadKebede@users.noreply.github.com>
2022-08-11 18:34:16 -0700

    Update 2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -7,6 +7,4 @@ CREATE USER
     IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT
    ON `hbtn_0d_2`.*
-   TO 'user_0d_2'@'localhost'
-   IDENTIFIED BY 'user_0d_2_pwd';
-FLUSH PRIVILEGES;
+   TO 'user_0d_2'@'localhost';
